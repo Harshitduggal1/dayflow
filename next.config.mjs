cf12d0e85d6295e3cda7a76f7e38fa212d9916d7
harshitduggal1 <harshitduggal56@gmail.com>
2024-10-03 21:55:00 +0530

    final commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+  'build/types/**/*.ts'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  experimental: {
+    outputFileTracingRoot: dirname(fileURLToPath(import.meta.url)), // Use this to get the directory name
+  },
   images: {
     remotePatterns: [
       { hostname: "avatar.vercel.sh", port: "", protocol: "https" },
       { hostname: "utfs.io", port: "", protocol: "https" },
-      {
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        protocol: "https",
-      },
+      { hostname: "avatars.githubusercontent.com", port: "", protocol: "https" },
     ],
   },
+  distDir: 'build', // Netlify can cache this directory
 };
 
 export default nextConfig;
